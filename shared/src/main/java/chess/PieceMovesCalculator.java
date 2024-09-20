beb83b2dea04a18dee4c2db8b94a683a881e98fc
evanscott1 <157188169+evanscott1@users.noreply.github.com>
2024-09-19 23:41:17 -0600

    Feat: scaffolding blackPawnMoves

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -19,6 +19,7 @@ public class PieceMovesCalculator {
 
     public ArrayList<ChessMove> calculateMoves(ChessBoard board, ChessPosition myPosition){
         ChessPiece.PieceType type = board.getPiece(myPosition).getPieceType();
+        ChessGame.TeamColor color = board.getPiece(myPosition).getTeamColor();
         switch (type) {
             case KING -> {
             }
@@ -32,7 +33,12 @@ public class PieceMovesCalculator {
             case ROOK -> {
             }
             case PAWN -> {
-                whitePawnMoves(board, myPosition);
+                if (color == ChessGame.TeamColor.WHITE) {
+                    whitePawnMoves(board, myPosition);
+                } else if (color == ChessGame.TeamColor.BLACK) {
+                    blackPawnMoves(board, myPosition);
+                }
+
             }
             default -> throw new IllegalStateException("Unexpected value: " + type);
         }
@@ -82,4 +88,44 @@ public class PieceMovesCalculator {
 
 
     }
+
+    public void blackPawnMoves(ChessBoard board, ChessPosition myPosition){
+
+        ChessPosition forwardOne = new ChessPosition(myPosition.getRow()-1, myPosition.getColumn());
+        ChessPosition forwardTwo = new ChessPosition(myPosition.getRow()-2, myPosition.getColumn());
+        ChessPosition forwardLeft = new ChessPosition(myPosition.getRow()-1, myPosition.getColumn()+1);
+        ChessPosition forwardRight = new ChessPosition(myPosition.getRow()-1, myPosition.getColumn()-1);
+
+
+
+        if(board.getPiece(forwardOne) == null) {
+            //Pawn promotion.
+            if(myPosition.getRow() == 2) {
+                for (ChessPiece.PieceType type : ChessPiece.PieceType.values()){
+                    if(type != ChessPiece.PieceType.KING && type != ChessPiece.PieceType.PAWN){
+                        addMove(myPosition, forwardOne, type);
+                    }
+                }
+            } else {
+                addMove(myPosition, forwardOne, null);
+                //Pawn starts.
+                if (myPosition.getRow() == 7 && board.getPiece((forwardTwo)) == null) {
+                    addMove(myPosition, forwardTwo, null);
+                }
+
+            }
+
+        }
+        //Pawn takes left diagonal.
+        if (board.getPiece(forwardLeft)!= null && board.getPiece(forwardLeft).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+            addMove(myPosition, forwardLeft, null);
+        }
+        //Pawn takes right diagonal.
+        if(board.getPiece(forwardRight) != null && board.getPiece(forwardRight).getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+            addMove(myPosition, forwardRight, null);
+        }
+
+
+    }
 }
+
