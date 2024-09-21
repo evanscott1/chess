58163f3494453a9ae45b8cd5ece955474c67bf1d
evanscott1 <157188169+evanscott1@users.noreply.github.com>
2024-09-20 18:49:06 -0600

    Feat: defaultGameboard  test

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,10 @@
 package chess;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,6 +12,19 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
+    }
+
     private final ChessPiece[][] squares = new ChessPiece[8][8];
     public ChessBoard() {
         
@@ -38,11 +56,78 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        placePawns(ChessGame.TeamColor.BLACK, 7);
+        placePawns(ChessGame.TeamColor.WHITE, 2);
+        royalPieces(ChessGame.TeamColor.BLACK, 8);
+        royalPieces(ChessGame.TeamColor.WHITE, 1);
+
+
+    }
+    public void placePawns(ChessGame.TeamColor color, int row) {
+        for(int i = 1; i <= getWidth(); i++) {
+            ChessPosition pawnPos = new ChessPosition(row, i);
+            ChessPiece pawn = new ChessPiece(color, ChessPiece.PieceType.PAWN);
+            addPiece(pawnPos, pawn);
+        }
+
+    }
+    public void royalPieces(ChessGame.TeamColor color, int row) {
+        ArrayList<ChessPiece.PieceType> types = new ArrayList<>();
+        types.add(ChessPiece.PieceType.ROOK);
+        types.add(ChessPiece.PieceType.KNIGHT);
+        types.add(ChessPiece.PieceType.BISHOP);
+        types.add(ChessPiece.PieceType.QUEEN);
+        types.add(ChessPiece.PieceType.KING);
+        types.add(ChessPiece.PieceType.BISHOP);
+        types.add(ChessPiece.PieceType.KNIGHT);
+        types.add(ChessPiece.PieceType.ROOK);
+
+        int i = 1;
+        for(ChessPiece.PieceType type : types) {
+            ChessPosition pos = new ChessPosition(row, i);
+            ChessPiece piece = new ChessPiece(color, type);
+            addPiece(pos, piece);
+            i++;
+        }
     }
 
     public int getWidth() {
         return squares.length;
+    }
+
+    public void printBoard(ChessBoard board) {
+        for (int i = 1; i <= getWidth(); i++) {
+            for (int j = 1; j <= getWidth(); j++) {
+                ChessPosition pos = new ChessPosition(i, j);
+                char c;
+                switch (board.getPiece(pos).getPieceType()) {
+                    case KING -> {
+                        c = 'K';
+                    }
+                    case PAWN -> {
+                        c = 'P';
+                    }
+                    case ROOK -> {
+                        c = 'R';
+                    }
+                    case QUEEN -> {
+                        c = 'Q';
+                    }
+                    case BISHOP ->  {
+                        c = 'B';
+                    }
+                    case KNIGHT -> {
+                        c = 'N';
+                    }
+                    case null -> {
+                        c = ' ';
+                    }
+                    default -> throw new IllegalStateException("Unexpected value: " + board.getPiece(pos));
+                }
+                System.out.print("|" + c);
+            }
+            System.out.println('|');
+        }
     }
 }
 
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -11,6 +12,20 @@ import java.util.Collection;
  */
 public class ChessPiece {
     private final ChessGame.TeamColor pieceColor;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
+
     private final PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
