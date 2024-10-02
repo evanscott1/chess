4e9732ef345affc0212982d9dc2b3b779837e734
evanscott1 <157188169+evanscott1@users.noreply.github.com>
2024-10-02 12:15:11 -0600

    Feat: Scaffolding GameState

--- shared/src/main/java/chess/GameState.java
+++ shared/src/main/java/chess/GameState.java
@@ -1,5 +1,25 @@
 package chess;
 
 public class GameState {
+    public GameState() {
 
+    }
+
+    public void turn() {
+
+    }
+
+    public void status() {
+
+    }
+
+    public void board() {
+
+    }
+
+    public void rules() {
+
+    }
+
+    
 }
