01778a7a4e5c5b38cd7622b146fa26579d11899a
evanscott1 <157188169+evanscott1@users.noreply.github.com>
2024-10-02 12:22:07 -0600

    Feat: Scaffolding ChessRuleBook

--- shared/src/main/java/chess/ChessRuleBook.java
+++ shared/src/main/java/chess/ChessRuleBook.java
@@ -1,4 +1,25 @@
 package chess;
 
+import java.util.Collection;
+
 public class ChessRuleBook {
+    public Collection<ChessMove> validMoves(ChessBoard board, ChessPosition position) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public boolean isBoardValid(ChessBoard board) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public boolean isInCheck(ChessBoard board, ChessGame.TeamColor color) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public boolean isInCheckmate(ChessBoard board, ChessGame.TeamColor color) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    public boolean isInStalemate(ChessBoard board, ChessGame.TeamColor color) {
+        throw new RuntimeException("Not implemented");
+    }
 }
