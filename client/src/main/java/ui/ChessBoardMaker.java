33c72e0fccd3534ab47b920dc6d91223144c6904
evanscott1 <157188169+evanscott1@users.noreply.github.com>
2024-11-04 19:10:10 -0700

    Feat: White Chess Board

--- client/src/main/java/ui/ChessBoardMaker.java
+++ client/src/main/java/ui/ChessBoardMaker.java
@@ -1,6 +1,7 @@
 package ui;
 
 import chess.ChessBoard;
+import chess.ChessGame;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import model.GameData;
@@ -26,6 +27,7 @@ public class ChessBoardMaker {
     private static final String O = " O ";
 
     private static Random rand = new Random();
+    private static SquareType startSquareType;
 
 //    private static final GameData gameData = null;
 
@@ -40,58 +42,97 @@ public class ChessBoardMaker {
 
         ArrayList<String> headers = new ArrayList<>(List.of(" ", "a", "b", "c", "d", "e", "f", "g", "h", " "));
 
-        squareCharacters.addAll(headers);
+//        squareCharacters.addAll(headers);
 
-        printLine(out);
-        for (String header : headers) {
-            printSquare(out, header);
-        }
-        printLine(out);
+        /*
+        Header
+         */
+        printHeader(out, headers);
 
-        for (int i = 1; i < BOARD_SIZE_IN_SQUARES - 1; i++) {
-            squareCharacters.add(Integer.toString(i));
-            printLine(out);
+
+        startSquareType = SquareType.LIGHT;
+
+        for (int i = BOARD_SIZE_IN_SQUARES - 2; i > 0; i--) {
+//            squareCharacters.add(Integer.toString(i));
+
+            /*
+            Row: First line
+             */
+            printBoardLine(out, startSquareType);
+
+            /*
+            Row: Second line
+             */
+            SquareType squareType = startSquareType;
+
+            setBorder(out);
             printSquare(out, Integer.toString(i));
 
-            for (int j = 1; j < BOARD_SIZE_IN_SQUARES - 1; j++) {
+            for (int j = BOARD_SIZE_IN_SQUARES - 2; j > 0; j--) {
                 String character;
                 ChessPiece piece = board.getPiece(new ChessPosition(i, j));
+                if (squareType == SquareType.LIGHT) {
+                    setLightBackground(out);
+                    squareType = SquareType.DARK;
+                } else {
+                    setDarkBackground(out);
+                    squareType = SquareType.LIGHT;
+                }
+
+
+
                 if (piece == null) {
                     character = " ";
                     printSquare(out, character);
                 } else {
+                    if (piece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+                        setLightTeam(out);
+                    } else if (piece.getTeamColor() == ChessGame.TeamColor.BLACK) {
+                        setDarkTeam(out);
+                    }
                     character = String.valueOf(piece.getPieceType().name().charAt(0));
                     printSquare(out, character);
                 }
 
-                squareCharacters.add(character);
+//                squareCharacters.add(character);
             }
 
-            squareCharacters.add(Integer.toString(i));
+//            squareCharacters.add(Integer.toString(i));
+            setBorder(out);
             printSquare(out, Integer.toString(i));
-            printLine(out);
-        }
-        printLine(out);
-        for (String header : headers) {
-            printSquare(out, header);
-        }
-        printLine(out);
-        squareCharacters.addAll(headers);
+            printNewLine(out);
 
+            /*
+            Row: Third line
+             */
+            printBoardLine(out, startSquareType);
+            if (startSquareType == SquareType.LIGHT) {
+                setLightBackground(out);
+                startSquareType = SquareType.DARK;
+            } else {
+                setDarkBackground(out);
+                startSquareType = SquareType.LIGHT;
+            }
 
-        for (int rows = 0; rows < BOARD_SIZE_IN_SQUARES; rows++) {
-
-        }
-
-
-
-        for (int square = 0; square < BOARD_SIZE_IN_SQUARES * BOARD_SIZE_IN_SQUARES; square++) {
 
         }
+
+
+        /*
+        Footer
+         */
+        printHeader(out, headers);
+
+
+//        squareCharacters.addAll(headers);
+
+
+
 
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
     }
+
 
 
     private static void printSquare(PrintStream out, String character) {
@@ -103,10 +144,71 @@ public class ChessBoardMaker {
         out.print(EMPTY.repeat(suffixLength));
     }
 
-    private static void printLine(PrintStream out) {
-        out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS * BOARD_SIZE_IN_SQUARES));
+    private static void printHeaderEmptyLine(PrintStream out) {
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        for (int i = 0; i < BOARD_SIZE_IN_SQUARES; i++){
+            printSquare(out, " ");
+        }
+        out.print(SET_BG_COLOR_BLACK);
         out.println();
     }
 
+    private static void printBoardLine(PrintStream out, SquareType sq) {
+        setBorder(out);
+        printEmptySquare(out);
+
+        for (int i = 0; i < BOARD_SIZE_IN_SQUARES - 2; i++) {
+            if (sq == SquareType.LIGHT) {
+                setLightBackground(out);
+                sq = SquareType.DARK;
+            } else if (sq == SquareType.DARK) {
+                setDarkBackground(out);
+                sq = SquareType.LIGHT;
+            }
+            printEmptySquare(out);
+        }
+        setBorder(out);
+        printEmptySquare(out);
+        printNewLine(out);
+    }
+
+    private static void printHeader(PrintStream out, ArrayList<String> headers) {
+        printHeaderEmptyLine(out);
+        setBorder(out);
+        for (String header : headers) {
+            printSquare(out, header);
+        }
+        printNewLine(out);
+        printHeaderEmptyLine(out);
+    }
+
+    private static void printEmptySquare(PrintStream out) {
+        printSquare(out, " ");
+    }
+
+    private static void printNewLine(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.println();
+    }
+
+    private static void setBorder(PrintStream out) {
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(SET_TEXT_COLOR_BLACK);
+    }
+
+    private static void setLightBackground(PrintStream out) {
+        out.print(SET_BG_COLOR_WHITE);
+    }
+
+    private static void setDarkBackground(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+    }
+    private static void setLightTeam(PrintStream out) {
+        out.print(SET_TEXT_COLOR_RED);
+    }
+
+    private static void setDarkTeam(PrintStream out) {
+        out.print(SET_TEXT_COLOR_BLUE);
+    }
 
 }
